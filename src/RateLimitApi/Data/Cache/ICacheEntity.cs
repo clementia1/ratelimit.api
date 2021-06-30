fe8569642ac96b50f235eebd39387f4cbe3a7317
clementia1 <grininma0@gmail.com>
2021-06-30 14:16:31 +0300

    remove unused cache entities

--- src/RateLimitApi/Data/Cache/ICacheEntity.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace RateLimitApi.Data.Cache
-{
-    public interface ICacheEntity
-    {
-        int Id { get; set; }
-    }
-}
--- src/RateLimitApi/Data/Cache/RequestCacheEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RateLimitApi.Data.Cache
-{
-    public class RequestCacheEntity
-    {
-        public string? ClientIp { get; set; }
-        public string? RequestedUrl { get; set; }
-        public string? RequestOrigin { get; set; }
-    }
-}
